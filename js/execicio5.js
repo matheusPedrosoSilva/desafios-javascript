722dd5abc9a1ca1d3a14ce9efbf5149d7f08a488
matheusp <matheus.pedroso.silva21@gmail.com>
2026-08-25 19:44:03 -0300

    feat:classifica situacao eleitoral

--- js/execicio5.js
+++ js/execicio5.js
@@ -1,4 +1,26 @@
-const btn = document.getElementById("desafio1");
-btn.addEventListener("click", () => {
+const btn5 = document.getElementById("desafio5");
+btn5.addEventListener("click", () => {
     main();
 });
+
+function main(){
+    let i = 'n'
+    while(i != 's'){
+        let idade = parseInt(prompt('Informe a idade:'))
+        if (idade < 16 && idade > 0){
+            window.alert('Não pode votar')
+            i = 's'
+        }
+        else if (idade >= 16 && idade <= 17){
+            window.alert('Voto opcional')
+            i = 's'
+        }
+        else if (idade >= 18){
+            window.alert('Voto obrigatório')
+            i = 's'
+        }
+        else{
+            window.alert('O valor é inválido, tente novamente')
+        }
+    }
+}
