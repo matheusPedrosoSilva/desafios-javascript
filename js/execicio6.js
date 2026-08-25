976072d55dfd812339c6d783322af26fa16f6f58
matheusp <matheus.pedroso.silva21@gmail.com>
2026-08-25 19:59:39 -0300

    feat:verifica situacao academica

--- js/execicio6.js
+++ js/execicio6.js
@@ -1,4 +1,26 @@
-const btn = document.getElementById("desafio1");
-btn.addEventListener("click", () => {
+const btn6 = document.getElementById("desafio6");
+btn6.addEventListener("click", () => {
     main();
 });
+
+function main(){
+    let n1 = parseFloat(prompt('Informe a primeira nota:'))
+    let n2 = parseFloat(prompt('Informe a segunda nota:'))
+    let media = (n1+n2)/2
+    let situacao = ''
+    if(media>=7&&media<=10){
+        situacao='Aprovado'
+        window.alert(`A média é : ${media.toFixed(2)} \nSituação: ${situacao}`)
+    }
+    else if (media<7&&media>=5){
+        situacao='Em recuperação'
+        window.alert(`A média é : ${media.toFixed(2)} \nSituação: ${situacao}`)
+    }
+    else if (media<5&&media>=0){
+        situacao='Reprovado'
+        window.alert(`A média é : ${media.toFixed(2)} \nSituação: ${situacao}`)
+    }
+    else{
+        window.alert('Os valores digitados são inválidos, por favor tente novamente')
+    }
+} 
