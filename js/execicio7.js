e2baac18a85ecc9e559c00eb88df42210f25fe25
matheusp <matheus.pedroso.silva21@gmail.com>
2026-08-25 20:08:18 -0300

    feat:verificao simples para login

--- js/execicio7.js
+++ js/execicio7.js
@@ -1,4 +1,22 @@
-const btn = document.getElementById("desafio1");
-btn.addEventListener("click", () => {
+const btn7 = document.getElementById("desafio7");
+btn7.addEventListener("click", () => {
     main();
 });
+
+function main(){
+    let usuario = prompt('Infome o usuário:')
+    let senha = prompt('Informe a senha:')
+    if (usuario==='admin'&&senha==='1234'){
+        window.alert('Acesso Permitido')
+    }else{
+        window.alert('Acesso Negado')
+    }
+}
+
+// ===
+// Este operador ira  verificar se os valores são iguais e se os tipos são iguais
+// Exemplo: 5  == '5'
+// Essa verificação é falsa, porque um valor é string e outro number.
+
+// =
+// Este operador apenas atribui um valor a algo, ele não faz comparação.
