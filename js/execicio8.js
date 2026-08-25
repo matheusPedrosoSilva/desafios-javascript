4fdb5efcd554c11fa85e98b434ad1bcaea106df8
matheusp <matheus.pedroso.silva21@gmail.com>
2026-08-25 20:22:25 -0300

    feat:gera tabua usando for

--- js/execicio8.js
+++ js/execicio8.js
@@ -1,4 +1,16 @@
-const btn = document.getElementById("desafio1");
-btn.addEventListener("click", () => {
+const btn8 = document.getElementById("desafio8");
+btn8.addEventListener("click", () => {
     main();
 });
+
+function main(){
+    let valor = parseFloat(prompt('Informe um valor:'))
+    if(isNaN(valor+1)){
+         console.log('Valor digitado inválido, tente novamente')
+    }
+    else{
+        for(let i = 1; i<11; i++){
+            console.log(`${valor} x ${i} = ${valor*i}`)            
+        }
+    }
+}
