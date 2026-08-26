e2f74bc224fa224b0cfb0e4a41e54ec40f3918f9
matheusp <matheus.pedroso.silva21@gmail.com>
2026-08-25 21:10:19 -0300

    feat:processa numeros com laco while

--- js/execicio9.js
+++ js/execicio9.js
@@ -1,4 +1,30 @@
-const btn = document.getElementById("desafio1");
-btn.addEventListener("click", () => {
+const btn9 = document.getElementById("desafio9");
+btn9.addEventListener("click", () => {
     main();
 });
+
+function main(){
+    let valor = 1
+    let numeros = []
+    let soma = 0
+    let min = null
+    let max = null
+
+    while(valor!=0){
+        valor = parseFloat(prompt('Informe um número'))
+        if (valor === 0||isNaN(valor)){
+            break
+        }
+        else{
+            numeros.push(valor)
+            soma += valor
+            if(valor<min||min===null){
+                min = valor
+            }
+            if(valor>max||max===null){
+                max = valor
+            }
+        }
+    }
+    console.log(`Soma dos números: ${soma} \nQuantidade de números: ${numeros.length} \nMédia entre os valores: ${(soma/numeros.length).toFixed(2)} \nMenor Valor: ${min} \nMaior valor: ${max}`)
+}
